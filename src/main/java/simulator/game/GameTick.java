96f70edc2ca92661c7b9337490b0e5b1e5748618
SomnusK <1024066060@qq.com>
2022-11-11 17:17:30 +0800

    update

--- src/main/java/simulator/game/GameTick.java
+++ src/main/java/simulator/game/GameTick.java
@@ -4,7 +4,12 @@ import simulator.game.*;
 
 public class GameTick {
     public MapGrid[] changes;
-    public String operator;
+    public char operator;
     public GameAction action;
     public boolean action_valid;
+    public void judge_operator(int num)
+    {
+        if(num%2==0) this.operator='R';
+        else this.operator='B';
+    }
 }
