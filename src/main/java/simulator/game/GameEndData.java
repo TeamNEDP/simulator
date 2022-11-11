fb7c0881187c9666618f3b6c52c3984a4787f46b
SomnusK <1024066060@qq.com>
2022-11-11 10:54:18 +0800

    update

---
+++ src/main/java/simulator/game/GameEndData.java
@@ -0,0 +1,12 @@
+package simulator.game;
+
+public class GameEndData {
+    public String id;
+    public GameResult result;
+    public GameEndData(String id_1,GameResult result_1)
+    {
+        id=id_1;
+        result=result_1;
+    }
+    
+}
