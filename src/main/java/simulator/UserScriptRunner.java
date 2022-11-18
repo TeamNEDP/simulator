432ae4549b5d90d27009a335d1f7a308c6729df7
Joky_02 <Joky_02@qq.com>
2022-11-18 10:04:47 +0800

    test

--- src/main/java/simulator/UserScriptRunner.java
+++ src/main/java/simulator/UserScriptRunner.java
@@ -31,7 +31,7 @@ public class UserScriptRunner {
 			noop = !Boolean.TRUE.equals(executeWithTimeout(() -> {
 				engine.eval(script.content);
 				return true;
-			}, 500, service));
+			}, TIME_LIMIT, service));
 		} else {
 			noop = true;
 		}
