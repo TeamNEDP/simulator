c4779c42edb9d5c6060ba2ba08e57cfbca0e4b5e
Joky_02 <Joky_02@qq.com>
2022-11-18 13:26:54 +0800

    add action_error

--- src/main/java/simulator/UserScriptRunner.java
+++ src/main/java/simulator/UserScriptRunner.java
@@ -44,23 +44,17 @@ public class UserScriptRunner {
 			return null;
 		}
 
-		var moveAction = executeWithTimeout(() -> {
+		return executeWithTimeout(() -> {
 					var res = engine.eval("Tick(\"" + color + "\", " + new Gson().toJson(stat) + ");");
 					if (res == null) {
 						return null;
 					}
 					return MoveAction.fromObject(res);
-				}, TIME_LIMIT, service
+				}, () -> tick.action_error = "time limit exceeded", TIME_LIMIT, service
 		);
-
-		if (moveAction == null) {
-			tick.action_error = "tle";
-		}
-
-		return moveAction;
 	}
 
-	private static <T> T executeWithTimeout(Callable<T> callable, @SuppressWarnings("SameParameterValue") long timeout, ExecutorService service) {
+	private static <T> T executeWithTimeout(Callable<T> callable, Runnable timeoutHandler, @SuppressWarnings("SameParameterValue") long timeout, ExecutorService service) {
 
 		var future = service.submit(callable);
 		var start = System.currentTimeMillis();
@@ -79,8 +73,14 @@ public class UserScriptRunner {
 		try {
 			return future.get();
 		} catch (InterruptedException | ExecutionException e) {
+			timeoutHandler.run();
 			return null;
 		}
+	}
+
+	private static <T> T executeWithTimeout(Callable<T> callable, @SuppressWarnings("SameParameterValue") long timeout, ExecutorService service) {
+		return executeWithTimeout(callable, () -> {
+		}, timeout, service);
 	}
 
 }
