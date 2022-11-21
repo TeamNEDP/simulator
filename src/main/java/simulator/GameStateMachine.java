6a7c21aafd90de5d64d2e7a49ec2e1a75096d0f9
Joky_02 <Joky_02@qq.com>
2022-11-21 16:29:54 +0800

    test

--- src/main/java/simulator/GameStateMachine.java
+++ src/main/java/simulator/GameStateMachine.java
@@ -59,7 +59,11 @@ public class GameStateMachine {
 			action = rRunner.run(stat, tick);
 		} else {
 			GameStat stat = GameStat.fromGameMap("B", currentGameState.gameMap);
+
+			System.out.println("var stat = " + new Gson().toJson(stat));
+
 			action = bRunner.run(stat, tick);
+			System.out.println("var moveAction = " + new Gson().toJson(action));
 		}
 
 		currentGameState.applyMoveAction(current == 1 ? "R" : "B", action, tick);
