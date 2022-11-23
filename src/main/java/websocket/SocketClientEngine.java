82da8c199b3c9b86c900408d1e46d0d74ad4b5cf
SeraphJACK <seraphjack@outlook.com>
2022-11-23 17:04:36 +0800

    auto reconnect

--- src/main/java/websocket/SocketClientEngine.java
+++ src/main/java/websocket/SocketClientEngine.java
@@ -5,38 +5,39 @@ import org.java_websocket.enums.ReadyState;
 import java.net.URI;
 import java.net.URISyntaxException;
 
-@SuppressWarnings("BusyWait")
+@SuppressWarnings({"BusyWait", "InfiniteLoopStatement"})
 public class SocketClientEngine {
 
-	public static void main(String[] args) throws URISyntaxException, InterruptedException { 
-		var url = System.getenv("SERVER_URL");
+    public static void main(String[] args) throws URISyntaxException, InterruptedException {
+        var url = System.getenv("SERVER_URL");
 
-		// token 发过去
-		var secret = System.getenv("SERVER_SECRET");
+        // token 发过去
+        var secret = System.getenv("SERVER_SECRET");
 
-		WebsocketHandler client;
+        WebsocketHandler client;
 
-		do {
-			client = new WebsocketHandler(new URI(url), secret);
-			client.connectBlocking();
-			System.out.println("Cannot connect. Retrying after 10 seconds.");
-			Thread.sleep(10000);
-		} while (!client.getReadyState().equals(ReadyState.OPEN));
+        client = new WebsocketHandler(new URI(url), secret);
+        client.connectBlocking();
 
-		System.out.println("Connected to websocket.");
+        if (client.getReadyState().equals(ReadyState.OPEN)) System.out.println("Connected to websocket.");
 
-		while (client.getReadyState().equals(ReadyState.OPEN)) {
-			if (client.getReadyState().equals(ReadyState.OPEN)) {
-				client.sendPing();
-				Thread.sleep(10000);
-			} else if (client.getReadyState().equals(ReadyState.CLOSED)) {
-				do {
-					client = new WebsocketHandler(new URI(url), secret);
-					client.connectBlocking();
-					System.out.println("Cannot connect. Retrying after 10 seconds.");
-					Thread.sleep(10000);
-				} while (!client.getReadyState().equals(ReadyState.OPEN));
-			}
-		}
-	}
+        for (; ; ) {
+            if (client.getReadyState().equals(ReadyState.OPEN)) {
+                client.sendPing();
+                Thread.sleep(10000);
+            } else if (client.getReadyState().equals(ReadyState.CLOSED)) {
+                System.out.println("Reconnecting...");
+                do {
+                    client = new WebsocketHandler(new URI(url), secret);
+                    client.connectBlocking();
+                    if (!client.getReadyState().equals(ReadyState.OPEN)) {
+                        System.out.println("Cannot connect. Retrying after 10 seconds.");
+                    } else {
+                        System.out.println("Connected to websocket.");
+                    }
+                    Thread.sleep(10000);
+                } while (!client.getReadyState().equals(ReadyState.OPEN));
+            }
+        }
+    }
 }
